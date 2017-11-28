999f848b26351dd7c0e4a8f216247021b4740ce4
ElderMayday <galdarsar@gmail.com>
2017-11-28 01:09:28 +0100

    Add SSIS project

---
+++ SQL/drop-snapshots.sql
@@ -0,0 +1,11 @@
+use [Solution]
+
+drop TABLE [newTitle];
+drop TABLE [newdepartments];
+drop TABLE [new_client_emp];
+drop TABLE [new_emp];
+drop TABLE [snapshotdates];
+drop TABLE [newCity];
+drop TABLE [newState];
+drop TABLE [newCountry];
+drop TABLE [new_clients];
