8b695278101460dd5aaa7b559226de38963e2f33
ElderMayday <galdarsar@gmail.com>
2017-12-03 03:52:55 +0100

    Add change city

--- SQL/create-temp.sql
+++ SQL/create-temp.sql
@@ -1,5 +1,62 @@
--- creates/recreates [temp] database, however the tables should be redefined during every iteration
+use [temp]
+go
 
-drop database [temp]
 
-create database [temp]
+CREATE TABLE [newdepartments] (
+    [dept_no] varchar(4),
+    [dept_name] varchar(100)
+);
+
+CREATE TABLE [newCity] (
+    [city_id] int,
+    [city] varchar(100),
+    [state_id] int
+);
+
+CREATE TABLE [newState] (
+    [state_id] int,
+    [state] varchar(100),
+    [country_id] int
+);
+
+CREATE TABLE [newCountry] (
+    [country] varchar(100),
+    [country_id] int
+);
+
+CREATE TABLE newTitle(
+	[id] [int] NOT NULL,
+	[title] [varchar](50) NULL
+);
+
+CREATE TABLE [new_client_emp] (
+    [emp_no] int,
+    [client] int
+);
+
+CREATE TABLE [new_emp] (
+    [emp_no] int,
+    [birth_date] datetime,
+    [first_name] varchar(100),
+    [last_name] varchar(100),
+    [gender] varchar(10),
+    [hire_date] datetime,
+    [salary] int,
+    [title_no] int,
+    [dept_no] varchar(4),
+    [current_city] int,
+    [base_city] int,
+    [marital_status] varchar(10),
+    [no_of_children] int
+);
+
+CREATE TABLE [snapshotdates] (
+    [from_date] datetime,
+    [to_date] datetime
+);
+
+CREATE TABLE [new_clients] (
+    [client_id] int,
+    [client] varchar(100),
+    [category] varchar(10)
+);
